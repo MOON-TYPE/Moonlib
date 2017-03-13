b2c694c9faec5633edbfc64cb11620b18cefb29a
MoonAntonio <pinchodev91@gmail.com>
2017-03-13 15:31:18 +0100

    Add script StringEx.cs

---
+++ Moonlib Project/Assets/MoonPincho/Extensiones/Genericas/StringEx.cs
@@ -0,0 +1,27 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// StringEx.cs (13/03/2017)														\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:		Extension de String											\\
+// Fecha Mod:		13/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+#endregion
+
+namespace MoonPincho
+{
+	/// <summary>
+	/// <para>Extension de String</para>
+	/// </summary>
+	public static class StringEx
+	{
+
+	}
+}
