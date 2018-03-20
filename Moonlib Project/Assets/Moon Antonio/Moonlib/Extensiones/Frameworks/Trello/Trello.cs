87e9633e127ccb570fe5b77e561f1a57a13c2c4a
Moon Antonio <pinchodev91@gmail.com>
2018-03-20 15:48:14 +0100

    Add Trello.cs

---
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Frameworks/Trello/Trello.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Trello.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Moonlib.Trello
+{
+	public class Trello : MonoBehaviour 
+	{
+		
+	}
+}
