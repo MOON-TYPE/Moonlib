b3bf9b9d2f1754c8b71f00cce8639f83a02a1768
Moon Antonio <pinchodev91@gmail.com>
2018-03-20 16:07:45 +0100

    Add TrelloException.cs

---
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Frameworks/Trello/TrelloException.cs
@@ -0,0 +1,48 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// TrelloException.cs (20/03/2018)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Clase base de excepciones para Trello.						\\
+// Fecha Mod:		20/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using System;
+#endregion
+
+namespace MoonAntonio.Moonlib.Trello
+{
+	/// <summary>
+	/// <para>Clase base de excepciones para Trello.</para>
+	/// </summary>
+	public class TrelloException : Exception
+	{
+		#region Metodos
+		public TrelloException() : base()
+		{
+
+		}
+
+		public TrelloException(string message) : base(message)
+		{
+
+		}
+
+		public TrelloException(string format, params object[] args) : base(string.Format(format, args))
+		{
+
+		}
+
+		public TrelloException(string message, Exception innerException) : base(message, innerException)
+		{
+
+		}
+
+		public TrelloException(string format, Exception innerException, params object[] args) : base(string.Format(format, args), innerException)
+		{
+
+		}
+		#endregion
+	}
+}
