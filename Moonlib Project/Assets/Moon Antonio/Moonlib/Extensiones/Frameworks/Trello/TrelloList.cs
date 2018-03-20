94045f7c94b15bddd8c3441d6c2f1366e57bba9a
Moon Antonio <pinchodev91@gmail.com>
2018-03-20 15:53:28 +0100

    Add TrelloList.cs

---
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Frameworks/Trello/TrelloList.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// TrelloList.cs (20/03/2018)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Clase base de las listas de trello.							\\
+// Fecha Mod:		20/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+namespace MoonAntonio.Moonlib.Trello
+{
+	/// <summary>
+	/// <para>Clase base de las listas de trello.</para>
+	/// </summary>
+	public class TrelloList
+	{
+		#region Variables Publicas
+		public string name = "";
+		public string idBoard = "";
+		public string pos = "bottom";
+		#endregion
+
+		#region Constructor
+		public TrelloList()
+		{
+
+		}
+		#endregion
+	}
+}
