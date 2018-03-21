71f2b328da2573041b35c63f899e71fb58e78f20
Moon Antonio <pinchodev91@gmail.com>
2018-03-21 13:11:38 +0100

    Add TypeArgumentException.cs

--- Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Excepciones/NotImplementedByException.cs
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Excepciones/NotImplementedByException.cs
@@ -22,6 +22,20 @@ namespace MoonAntonio.Moonlib
 		/// <para>Inicializa una nueva instancia de <see cref="NotImplementedByException"/>.</para>
 		/// </summary>
 		/// <param name="type">El tipo de la clase que arroja esta excepcion.</param>
+		/// <code>
+		/// [Abstract]
+		/// public class BaseClass
+		/// {
+		///		[Abstract]
+		///		public virtual void Method()
+		///		{
+		///			throw new NotImplementedBy(GetType());
+		///		}
+		/// }
+		/// 
+		/// public class DerivedClass : BaseClass { }
+		/// </code>
+		/// </example>
 		public NotImplementedByException(Type type) : base("No implementado por " + type) // Inicializa una nueva instancia de NotImplementedByException
 		{
 
---
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Excepciones/TypeArgumentException.cs
@@ -0,0 +1,33 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// TypeArgumentException.cs (21/03/2018)										\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Una version de TypeArgumentException. 						\\
+// Fecha Mod:		21/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using System;
+#endregion
+
+namespace MoonAntonio.Moonlib
+{
+	/// <summary>
+	/// <para>Una version de TypeArgumentException.</para>
+	/// </summary>
+	public class TypeArgumentException : Exception 
+	{
+		public readonly string parameterName;
+
+		public TypeArgumentException(string message) : base(message)
+		{
+			parameterName = "";
+		}
+
+		public TypeArgumentException(string parameterName, string message) : base(string.Format("{0}\nNombre Parametro: {1}", message, parameterName))
+		{
+			this.parameterName = parameterName;
+		}
+	}
+}
