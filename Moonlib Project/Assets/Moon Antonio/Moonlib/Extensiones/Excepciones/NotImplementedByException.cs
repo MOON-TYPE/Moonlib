dd4fc2020ce71b259e1058806746311ea0c8c6f9
Moon Antonio <pinchodev91@gmail.com>
2018-03-21 13:07:49 +0100

    Add NotImplementedByException.cs

---
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Excepciones/NotImplementedByException.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// NotImplementedByException.cs (21/03/2018)									\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Una version de NotImplementedException.						\\
+// Fecha Mod:		21/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using System;
+#endregion
+
+namespace MoonAntonio.Moonlib
+{
+	/// <summary>
+	/// <para>Una version de NotImplementedException.</para>
+	/// </summary>
+	public class NotImplementedByException : NotImplementedException
+	{
+		/// <summary>
+		/// <para>Inicializa una nueva instancia de <see cref="NotImplementedByException"/>.</para>
+		/// </summary>
+		/// <param name="type">El tipo de la clase que arroja esta excepcion.</param>
+		public NotImplementedByException(Type type) : base("No implementado por " + type) // Inicializa una nueva instancia de NotImplementedByException
+		{
+
+		}
+	}
+}
