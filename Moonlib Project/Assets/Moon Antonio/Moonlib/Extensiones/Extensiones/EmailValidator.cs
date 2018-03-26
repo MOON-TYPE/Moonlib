40386b6a8c9f545f3f576127b4875d9808d9176f
Moon Antonio <pinchodev91@gmail.com>
2018-03-26 19:31:58 +0200

    Add EmailValidator.cs

---
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Extensiones/EmailValidator.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// EmailValidator.cs (26/03/2018)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Ayudante validador de un correo electronico.				\\
+// Fecha Mod:		26/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+#endregion
+
+namespace MoonAntonio.Moonlib.Extensiones
+{
+	/// <summary>
+	/// <para>Ayudante validador de un correo electronico.</para>
+	/// </summary>
+	internal class EmailValidator
+	{
+		#region Variables Privadas
+		/// <summary>
+		/// <para>True si el email es valido.</para>
+		/// </summary>
+		private bool invalid;									// True si el email es valido
+		#endregion
+	}
+}
