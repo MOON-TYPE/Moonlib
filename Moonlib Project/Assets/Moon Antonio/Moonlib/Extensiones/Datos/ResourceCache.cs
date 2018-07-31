b8c41ecd555a43d2e0f72c2231b4587be4e340c6
Moon Antonio <pinchodev91@gmail.com>
2018-07-31 22:33:08 +0200

    Preparando ResourceCache

---
+++ Moonlib Project/Assets/Moon Antonio/Moonlib/Extensiones/Datos/ResourceCache.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ResourceCache.cs (31/07/2018)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Ayudante cache Resources.Load aumenta 2x el aumento del		\\
+//					rendimiento.												\\
+// Fecha Mod:		31/07/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Moonlib
+{
+	/// <summary>
+	/// <para>Ayudante cache Resources.Load aumenta 2x el aumento del rendimiento.</para>
+	/// </summary>
+	sealed class ResourceCache : MonoBehaviourServicios<ResourceCache>
+	{
+
+	}
+}
